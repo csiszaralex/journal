c5eb587e3617a3f7365c4c77ad32760c4c87ee52
Alex Csiszar <csiszaralex@gmail.com>
2026-04-21 21:21:27 +0200

    fix: add statement-breakpoint comment in app_settings SQL file

--- drizzle/0003_organic_sister_grimm.sql
+++ drizzle/0003_organic_sister_grimm.sql
@@ -2,4 +2,5 @@ CREATE TABLE `app_settings` (
 	`key` text PRIMARY KEY NOT NULL,
 	`value` text NOT NULL
 );
+--> statement-breakpoint
 INSERT INTO `app_settings` (`key`, `value`) VALUES ('registration_enabled', 'true');
